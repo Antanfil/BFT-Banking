88ba7b849dd560042d4e57ff037cf5733301fadc
ist192510 <ist192510@tecnico.ulisboa.pt>
2022-03-24 13:14:26 +0000

    KeyStore ainda não testado

--- client/src/main/java/pt/tecnico/sec/ClientMain.java
+++ client/src/main/java/pt/tecnico/sec/ClientMain.java
@@ -1,27 +1,27 @@
 package pt.tecnico.sec;
  
- import java.io.BufferedReader;
+ import java.io.*;
 
  import pt.tecnico.sec.server.grpc.*;
  import pt.tecnico.sec.server.grpc.Server.*;
  import pt.tecnico.sec.server.ServerFrontend;
 
- import java.security.KeyStore;
- import java.security.PublicKey;
+ import java.security.*;
+ import java.security.KeyStore.PasswordProtection;
+ import java.security.KeyStore.PrivateKeyEntry;
+ import java.security.cert.CertificateException;
+ import java.security.cert.X509Certificate;
  import java.util.List;
  import java.util.StringTokenizer;
- import java.io.InputStreamReader;
  import java.util.ArrayList;
- import java.io.IOException;
- 
- public class ClientMain {
+
+public class ClientMain {
  	
- 	public static void main(String[] args) throws NumberFormatException, InterruptedException{
+ 	public static void main(String[] args) throws NumberFormatException, InterruptedException, NoSuchProviderException, NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, UnrecoverableEntryException {
  		System.out.println(ClientMain.class.getSimpleName());
 
-		KeyStore _keystore;
+		KeyManagement _keyStore;
 
-		 		
  		// receive and print arguments
  		System.out.printf("Received %d arguments%n ", args.length);
  		for (int i = 0; i < args.length; i++) {
@@ -38,13 +38,14 @@ package pt.tecnico.sec;
 
  		ServerFrontend frontend = null ;
 		frontend = new ServerFrontend( host , port );
+
  		Client client = new Client( frontend );
 
+		String command;
 		StringTokenizer st;
 		StringTokenizer st1;
 		List<String> tokens = new ArrayList<>();
 		List<String> tokens1 = new ArrayList<>();
-		String command;
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		BufferedReader input1 = new BufferedReader(new InputStreamReader(System.in));
 
@@ -66,6 +67,78 @@ package pt.tecnico.sec;
 			System.out.println("Welcome to your Home banking Client No " + clientID + "\n");
 			System.out.println("Generating Client's Public/Private Key Pair ... \n");
 			// TODO
+
+			_keyStore = new KeyManagement();
+
+			/*//create the key pair
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+			keyGen.initialize(1024, random);
+			KeyPair pair = keyGen.generateKeyPair();
+			PrivateKey privateKey = pair.getPrivate();
+			PublicKey pub = pair.getPublic();
+
+			//create keystore
+
+			//FileInputStream store = new FileInputStream("store.keystore");
+
+			File file = new File("store");
+			KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+
+			if (!file.exists()) {
+				keystore.load(null, null);
+				FileOutputStream storeOut = new FileOutputStream("store.keystore");
+				keystore.store(storeOut, "password".toCharArray());
+			}
+			else {
+				FileInputStream storeIn = new FileInputStream("store.keystore");
+				keystore.load(storeIn, "password".toCharArray());
+			}
+
+			if (!keystore.containsAlias("alias")) {
+				keystore.setEntry("alias", new KeyStore.PrivateKeyEntry(privateKey, null), new KeyStore.PasswordProtection("pass".toCharArray()));	//chain null, mas precisa do certificado
+				FileOutputStream storeOut = new FileOutputStream("store.keystore");
+				keystore.store(storeOut, "pass".toCharArray());
+			}
+
+
+			keystore.load(store, "my-keystore-password".toCharArray());
+
+			keystore.setKeyEntry("privateAlias", priv,"password".toCharArray(), null);	//chain null(?)
+
+			keystore.load(store, "my-keystore-password".toCharArray());
+
+			String alias = "myalias";
+
+			Key key = keystore.getKey(alias, "password".toCharArray());
+			if (key instanceof PrivateKey) {
+				// Get certificate of public key
+				Certificate cert = keystore.getCertificate(alias);
+
+				// Get public key
+				PublicKey publicKey = cert.getPublicKey();
+
+				// Return a key pair
+				new KeyPair(publicKey, (PrivateKey) key);
+
+			KeyStore keyStore = KeyStore.getInstance("PKCS12");
+			char[] password = "my-keystore-password".toCharArray();
+
+			FileInputStream store = new FileInputStream("store.keystore");
+			keyStore.load(store, password);
+			KeyStore.ProtectionParameter protectionParam = new PasswordProtection(password);
+
+			char[] keyPassword = "789xyz".toCharArray();
+			KeyStore.ProtectionParameter entryPassword =
+					new PasswordProtection(keyPassword);
+
+			KeyStore.Entry keyEntry = keyStore.getEntry("keyAlias", entryPassword);
+
+			PrivateKeyEntry privateKeyEntry = (PrivateKeyEntry)
+					keyStore.getEntry("keyAlias", entryPassword);*/
+
+
+
 			System.out.println("Done !\n");
 			System.out.println("Connecting to server ...\n");
 			// TODO
@@ -104,12 +177,10 @@ package pt.tecnico.sec;
 							break;
 						case "6":
 							again = false;
-							System.out.println(again);
 							break;
 						default:
 							break;
 					}
-					System.out.println(again);
 				} catch (IOException e) {
 					System.out.println(e);
 				}
