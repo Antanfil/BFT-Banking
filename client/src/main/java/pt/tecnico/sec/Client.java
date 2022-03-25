7c303bb8fa326b521ee60364484cf78a7e16cdc9
Antonio Filipe <a.angeja.filipe@gmail.com>
2022-03-25 15:18:37 +0000

    Keystore

--- client/src/main/java/pt/tecnico/sec/Client.java
+++ client/src/main/java/pt/tecnico/sec/Client.java
@@ -1,7 +1,15 @@
 package pt.tecnico.sec;
 
-import java.security.KeyStore;
+import java.io.*;
+import java.security.*;
+import java.security.cert.Certificate;
 import java.util.*;
+
+import java.security.*;
+import java.security.KeyStore.PasswordProtection;
+import java.security.KeyStore.PrivateKeyEntry;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import pt.tecnico.sec.server.ServerFrontend;
 
@@ -9,18 +17,15 @@ public class Client {
 
     ServerFrontend _frontend;
     private int id;
-    private List<Account> accounts ;
+    KeyStore  keyStore;
 
-    String pbkAlias;
-    String prkAlias;
+    private String pbkAlias;
+    private String prkAlias;
 
-    public Client( ServerFrontend frontend){
+    public Client( ServerFrontend frontend , int id){
         _frontend = frontend;
+        this.id = id;
     }
-
-   /* public int askForBalance(){
-        return _frontend.Balance(_nickName);
-    }*/
 
 
     public boolean pingWorking( ){
@@ -31,4 +36,97 @@ public class Client {
         return true;
     }
 
+    public void createKeyStore() {
+
+        pbkAlias = "pbkAlias" + Integer.toString(id) ;
+        prkAlias = "prkAlias" + Integer.toString(id) ;
+        try {
+
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
+            //SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            keyGen.initialize(1024 );
+            KeyPair pair = keyGen.generateKeyPair();
+            PrivateKey privateKey = pair.getPrivate();
+            PublicKey publicKey = pair.getPublic();
+
+
+
+            File file = new File("store" + Integer.toString(id) +".jks" );
+
+            if( file.createNewFile() ){
+                keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+                keyStore.load(null, "password".toCharArray() );
+                FileOutputStream storeOut = new FileOutputStream("store" + Integer.toString(id) +".jks" );
+                keyStore.store(storeOut, "password".toCharArray());
+            }
+            else{
+                keyStore = KeyStore.getInstance("JKS");
+                keyStore.load(new FileInputStream("store" + Integer.toString(id) +".jks"), "password".toCharArray());
+            }
+
+            if ( !keyStore.containsAlias(prkAlias) && !keyStore.containsAlias(pbkAlias) ) {
+                //keystore.setEntry("alias", new KeyStore.PrivateKeyEntry(privateKey, null), new KeyStore.PasswordProtection("pass".toCharArray()));	//chain null, mas precisa do certificado
+                X509Certificate cert = new Certificate() ;
+                X509CertInfo info = new X509CertInfo();
+                keyStore.setKeyEntry( prkAlias, privateKey, "password".toCharArray() , null);
+                //keyStore.setKeyEntry( prkAlias , privateKey,"password".toCharArray(), null);//chain null(?)
+            }
+
+            /*
+    Date from = new Date();
+    Date to = new Date(from.getTime() + days * 86400000l);
+    CertificateValidity interval = new CertificateValidity(from, to);
+    BigInteger sn = new BigInteger(64, new SecureRandom());
+    X500Name owner = new X500Name(dn);
+
+    info.set(X509CertInfo.VALIDITY, interval);
+    info.set(X509CertInfo.SERIAL_NUMBER, new CertificateSerialNumber(sn));
+    info.set(X509CertInfo.SUBJECT, new CertificateSubjectName(owner));
+    info.set(X509CertInfo.ISSUER, new CertificateIssuerName(owner));
+    info.set(X509CertInfo.KEY, new CertificateX509Key(pair.getPublic()));
+    info.set(X509CertInfo.VERSION, new CertificateVersion(CertificateVersion.V3));
+    AlgorithmId algo = new AlgorithmId(AlgorithmId.md5WithRSAEncryption_oid);
+    info.set(X509CertInfo.ALGORITHM_ID, new CertificateAlgorithmId(algo));
+
+    // Sign the cert to identify the algorithm that's used.
+    X509CertImpl cert = new X509CertImpl(info);
+    cert.sign(privkey, algorithm);
+
+    // Update the algorith, and resign.
+    algo = (AlgorithmId)cert.get(X509CertImpl.SIG_ALG);
+    info.set(CertificateAlgorithmId.NAME + "." + CertificateAlgorithmId.ALGORITHM, algo);
+    cert = new X509CertImpl(info);
+    cert.sign(privkey, algorithm);*/
+            saveKeyStore();
+
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void saveKeyStore(){
+        FileOutputStream storeOut = null;
+        try {
+            storeOut = new FileOutputStream("store" + Integer.toString(id) +".jks" );
+            keyStore.store(storeOut, "password".toCharArray());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- client/src/main/java/pt/tecnico/sec/ClientMain.java
+++ client/src/main/java/pt/tecnico/sec/ClientMain.java
@@ -20,8 +20,6 @@ public class ClientMain {
  	public static void main(String[] args) throws NumberFormatException, InterruptedException, NoSuchProviderException, NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, UnrecoverableEntryException {
  		System.out.println(ClientMain.class.getSimpleName());
 
-		KeyManagement _keyStore;
-
  		// receive and print arguments
  		System.out.printf("Received %d arguments%n ", args.length);
  		for (int i = 0; i < args.length; i++) {
@@ -39,7 +37,6 @@ public class ClientMain {
  		ServerFrontend frontend = null ;
 		frontend = new ServerFrontend( host , port );
 
- 		Client client = new Client( frontend );
 
 		String command;
 		StringTokenizer st;
@@ -49,146 +46,80 @@ public class ClientMain {
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		BufferedReader input1 = new BufferedReader(new InputStreamReader(System.in));
 
-		do{
-			again = true;
-			System.out.println("Please insert your client id:");
+
+		again = true;
+		System.out.println("Please insert your client id:");
+		try {
+			System.out.printf("> ");
+			tokens1.clear();
+			st1 = new StringTokenizer(input1.readLine());
+			while (st1 != null && st1.hasMoreElements()) {
+				tokens1.add(st1.nextToken());
+			}
+			clientID = Integer.parseInt(tokens1.get(0));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Client client = new Client( frontend , clientID);
+
+		System.out.println("Welcome to your Home banking Client No " + clientID + "\n");
+		System.out.println("Generating Client's Public/Private Key Pair ... \n");
+
+
+		client.createKeyStore();
+
+		System.out.println("Done !\n");
+		System.out.println("Connecting to server ...\n");
+		// TODO
+		System.out.println("Connection to server complete !! We are ready to go. \n");
+
+		do {
 			try {
+				System.out.println("Select an action to perform: \n" +
+						"1.- Open an account\n" +
+						"2.- Send amount (source account number & destination account number & amount needed)\n" +
+						"3.- Check account (account number needed ) \n" +
+						"4.- Receive amount (account number needed) \n" +
+						"5.- Audit (account number needed)  \n" +
+						"6.- Exit\n");
 				System.out.printf("> ");
-				tokens1.clear();
-				st1 = new StringTokenizer(input1.readLine());
-				while (st1 != null && st1.hasMoreElements()) {
-					tokens1.add(st1.nextToken());
+				tokens.clear();
+				st = new StringTokenizer(input.readLine());
+				while (st != null && st.hasMoreElements()) {
+					tokens.add(st.nextToken());
 				}
-				clientID = Integer.parseInt(tokens1.get(0));
+				command = tokens.get(0);
+				System.out.println(command);
+				switch (command) {
+					case "ping":
+						client.pingWorking();
+						break;
+					case "1":
+						break;
+					case "2":
+						break;
+					case "3":
+						break;
+					case "4":
+						break;
+					case "5":
+						break;
+					case "6":
+						again = false;
+						break;
+					default:
+						break;
+				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println(e);
 			}
-
-			System.out.println("Welcome to your Home banking Client No " + clientID + "\n");
-			System.out.println("Generating Client's Public/Private Key Pair ... \n");
-			// TODO
-
-			_keyStore = new KeyManagement();
-
-			/*//create the key pair
-			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
-			SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-			keyGen.initialize(1024, random);
-			KeyPair pair = keyGen.generateKeyPair();
-			PrivateKey privateKey = pair.getPrivate();
-			PublicKey pub = pair.getPublic();
-
-			//create keystore
-
-			//FileInputStream store = new FileInputStream("store.keystore");
-
-			File file = new File("store");
-			KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-
-			if (!file.exists()) {
-				keystore.load(null, null);
-				FileOutputStream storeOut = new FileOutputStream("store.keystore");
-				keystore.store(storeOut, "password".toCharArray());
-			}
-			else {
-				FileInputStream storeIn = new FileInputStream("store.keystore");
-				keystore.load(storeIn, "password".toCharArray());
-			}
-
-			if (!keystore.containsAlias("alias")) {
-				keystore.setEntry("alias", new KeyStore.PrivateKeyEntry(privateKey, null), new KeyStore.PasswordProtection("pass".toCharArray()));	//chain null, mas precisa do certificado
-				FileOutputStream storeOut = new FileOutputStream("store.keystore");
-				keystore.store(storeOut, "pass".toCharArray());
-			}
+		} while (again );
 
 
-			keystore.load(store, "my-keystore-password".toCharArray());
-
-			keystore.setKeyEntry("privateAlias", priv,"password".toCharArray(), null);	//chain null(?)
-
-			keystore.load(store, "my-keystore-password".toCharArray());
-
-			String alias = "myalias";
-
-			Key key = keystore.getKey(alias, "password".toCharArray());
-			if (key instanceof PrivateKey) {
-				// Get certificate of public key
-				Certificate cert = keystore.getCertificate(alias);
-
-				// Get public key
-				PublicKey publicKey = cert.getPublicKey();
-
-				// Return a key pair
-				new KeyPair(publicKey, (PrivateKey) key);
-
-			KeyStore keyStore = KeyStore.getInstance("PKCS12");
-			char[] password = "my-keystore-password".toCharArray();
-
-			FileInputStream store = new FileInputStream("store.keystore");
-			keyStore.load(store, password);
-			KeyStore.ProtectionParameter protectionParam = new PasswordProtection(password);
-
-			char[] keyPassword = "789xyz".toCharArray();
-			KeyStore.ProtectionParameter entryPassword =
-					new PasswordProtection(keyPassword);
-
-			KeyStore.Entry keyEntry = keyStore.getEntry("keyAlias", entryPassword);
-
-			PrivateKeyEntry privateKeyEntry = (PrivateKeyEntry)
-					keyStore.getEntry("keyAlias", entryPassword);*/
+		System.out.println(" Goodbye!! Se you soon. \n");
 
 
-
-			System.out.println("Done !\n");
-			System.out.println("Connecting to server ...\n");
-			// TODO
-			System.out.println("Connection to server complete !! We are ready to go. \n");
-
-			do {
-				try {
-					System.out.println("Select an action to perform: \n" +
-							"1.- Open an account\n" +
-							"2.- Send amount (source account number & destination account number & amount needed)\n" +
-							"3.- Check account (account number needed ) \n" +
-							"4.- Receive amount (account number needed) \n" +
-							"5.- Audit (account number needed)  \n" +
-							"6.- Exit\n");
-					System.out.printf("> ");
-					tokens.clear();
-					st = new StringTokenizer(input.readLine());
-					while (st != null && st.hasMoreElements()) {
-						tokens.add(st.nextToken());
-					}
-					command = tokens.get(0);
-					System.out.println(command);
-					switch (command) {
-						case "ping":
-							client.pingWorking();
-							break;
-						case "1":
-							break;
-						case "2":
-							break;
-						case "3":
-							break;
-						case "4":
-							break;
-						case "5":
-							break;
-						case "6":
-							again = false;
-							break;
-						default:
-							break;
-					}
-				} catch (IOException e) {
-					System.out.println(e);
-				}
-			} while (again );
-
-			System.out.println(" Goodbye!! Se you soon. \n");
-
-		}while ( true );
  	}
  
  }
